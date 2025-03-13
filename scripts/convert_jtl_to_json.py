81ffa0e9408a000604f4e2c337d0f4e0e6f4c800
DESKTOP-UTMU2KI\Itthi <gm_boon@hotmail.com>
2025-03-14 00:08:22 +0700

    Wrapping json after converted

--- scripts/convert_jtl_to_json.py
+++ scripts/convert_jtl_to_json.py
@@ -5,7 +5,9 @@ def jtl_to_json(jtl_file):
     with open(jtl_file, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         result = [row for row in reader]
-    return json.dumps(result, indent=4)
+    
+    # Wrap the result in a dictionary with a 'logs' key
+    return json.dumps({"logs": result}, indent=4)
 
 # Write to JSON file
 with open('results/result.json', 'w', encoding='utf-8') as json_file:
